e151b7db1573aeb015abd686b31bef647222ed7a
VostokVladimir <vostokirk@mail.ru>
2022-04-26 14:06:42 +0300

    Выделен отдельные классы
    классs GameController для кнопки Bonus и PlayerBonusCatchControl для Игрока . Проблема в том что обработчик события не срабатывает и данные созданного игрока не поступают в файл сохранения.

--- SaveDataProject/Assets/Scripts/Controller/GameController.cs
+++ SaveDataProject/Assets/Scripts/Controller/GameController.cs
@@ -5,68 +5,31 @@ using UnityEngine.UI;
 
 namespace OOP
 {
-    public class GameController : MonoBehaviour
+    public class GameController :MonoBehaviour
     {
-        
-        private References references;
+        public PlayerBonusCatchControl catchControl;
         public PlayerInfo playerInfo;
-        public float score;
-        //public SaveGame game;
-       
         
-         
-
-        
-         
-
+                 
         public void Awake()
 
 
         {
-            playerInfo = new PlayerInfo();
-            playerInfo._NamePlayer = "Коля";
-            playerInfo.PositionPlayer = transform.position;
-            playerInfo.PositionPlayer.X = transform.position.x;
-            playerInfo.PositionPlayer.Y = transform.position.y;
-            playerInfo.PositionPlayer.Z = transform.position.z;
-            //задаем стартовую позицию игрока
-            Debug.Log($"позиция на старте X {playerInfo.PositionPlayer.X} Y {playerInfo.PositionPlayer.Y} Z {playerInfo.PositionPlayer.Z}");//проблема 1 почему выводит в дебаг  два варианта значения позиции объекта ? ( см принт скрин)
+            PlayerBonusCatchControl playerBonusCatchControl = new PlayerBonusCatchControl();
+            playerBonusCatchControl._greatePlayerevent += PlayerBonusCatchControl__greatePlayerevent;
             
-
-            #region "Работа с ссылками"
-            //вар 1
-            // GameObject playerGO = null; //GO обнуляем что бы скачать префаб с папки ресурсес
-            // references = new References();//создаем экз класса для хранения ссылок на папку//
-            // playerGO = references.Player;//блин говорит что ссылка пустая референсес не загрузился
-            //вар 2
-            //var _playerGO = Resources.Load<GameObject>("Player"); //дал прямую ссылку 
-            //Object.Instantiate(_player, new Vector3(2, 4, 2), Quaternion.identity);//грузится куча клонов префабов и юнити вылетает
-            //забить пока на загрузку из папки
-
-            //var player = new Player("Федор",20,0);
-
-            //playerInfo = new PlayerInfo("Lena", 34, 0);
-            // var name1 = playerInfo._NamePlayer;
-            #endregion
-
-                      
             
         }
 
-        public void OnTriggerEnter(Collider other)
+        private PlayerInfo PlayerBonusCatchControl__greatePlayerevent(PlayerInfo player)
         {
-            if(other.gameObject.CompareTag("Bonus"))
-
-            {
-                
-             var _goodbonus= other.GetComponent<GoodBonus>();
-             var bonus= _goodbonus.Point;//получаем размер очков от сбора Бонуса
-             playerInfo._Playerscore += bonus;
-             Debug.Log($" стало {playerInfo._Playerscore}");
-            }
+            playerInfo = player;
+            return playerInfo;
+            Debug.LogWarning($"Событие сработало создан обьект {playerInfo}");
         }
 
-       
+        
+
 
         void Update()
         {
---
+++ SaveDataProject/Assets/Scripts/View/CameraScrinShotController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScrinShotController : MonoBehaviour
+{
+    public Camera screenShotCamera;
+    
+
+    public void MakeScreenShot()
+    {
+        int width = this.screenShotCamera.pixelWidth;
+        int height = this.screenShotCamera.pixelHeight;
+        Texture2D texture = new Texture2D(width, height);
+        RenderTexture renderTexture = RenderTexture.GetTemporary(width, height);
+        this.screenShotCamera.targetTexture = renderTexture;//целевая рендер текстура куда камера рендерит изображение
+        this.screenShotCamera.Render();//единоразовый вызов рендера для снимка сцены
+        RenderTexture.active = renderTexture;//необходимо поместить целевую текстуру targetTexture, которую мы брали из архива, обратно, так как теперь очередь главной камеры “рендерить” сцену.
+        Rect rect = new Rect(0, 0, width, height);//В переменной targetTexture хранится изображение с новой камеры, пока главная камера еще не успела поместить в эту же переменную свое изображение, необходимо перенести изображение из RenderTexture в обычную текстуру Texture2D.
+        texture.ReadPixels(rect, 0, 0);
+        texture.Apply();
+      // Вопрос: //Как вывести изображение на элемент канваса Radar/Image?
+          
+    }
+
+}
